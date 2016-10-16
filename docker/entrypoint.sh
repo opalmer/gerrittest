88fe7055c797b48e50abf5d932bdabc7ebec8db6
Oliver Palmer <oliverpalmer@opalmer.com>
2016-10-16 11:52:18 -0400

    fix path to gerrit.war, add -x to shebang

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 set_gerrit_config() {
   git config -f "${GERRIT_SITE}/etc/gerrit.config" "$@"
@@ -6,6 +6,6 @@ set_gerrit_config() {
 
 set_gerrit_config gerrit.canonicalWebUrl ${GERRIT_CANONICAL_URL}
 
-java -jar ${GERRIT_WAR} init --batch --no-auto-start -d ${GERRIT_SITE}
-exec java -jar ${GERRIT_WAR} daemon --console-log -d ${GERRIT_SITE}
+java -jar ${GERRIT_HOME}/gerrit.war init --batch --no-auto-start -d ${GERRIT_SITE}
+exec java -jar ${GERRIT_HOME}/gerrit.war daemon --console-log -d ${GERRIT_SITE}
 
