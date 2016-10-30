ed3fdc0aae8f25888b41e208d3ad0c24850977e4
Oliver Palmer <oliverpalmer@opalmer.com>
2016-10-30 08:26:37 -0400

    adding test script

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+# This file is responsible for testing gerrittest on Travis. The docker image
+# must be built before this script can be run.
+
+DOCKER_IMAGE=opalmer/gerrittest:travis
+
+. ./docker/helper-functions.sh
+
+RunContainer
+CreateAdminAccount
+private_key=$(GenerateSSHPrivateKey)
+AddAdminSSHKey $private_key
