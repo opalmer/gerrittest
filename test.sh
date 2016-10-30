292fdaaa097adfaef3504a68a68cedc9dc6d0ba8
Oliver Palmer <oliverpalmer@opalmer.com>
2016-10-30 08:33:09 -0400

    use latest tag so we pull from docker

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 # This file is responsible for testing gerrittest on Travis. The docker image
 # must be built before this script can be run.
 
-DOCKER_IMAGE=opalmer/gerrittest:travis
+DOCKER_IMAGE=opalmer/gerrittest:latest
 
 . ./docker/helper-functions.sh
 
