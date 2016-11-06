ff7d066aa99330cb54fb6b3b5f39f83019117d88
Oliver Palmer <oliverpalmer@opalmer.com>
2016-11-06 14:05:55 -0500

    added destroy command

--- cmd/main.go
+++ cmd/main.go
@@ -65,6 +65,47 @@ var (
 			printinfo(created)
 			return nil
 		}}
+
+	// DestroyCommand is used to destroy containers related to gerrrittest.
+	DestroyCommand = &cobra.Command{
+		Use:   "destroy",
+		Short: "Destroys containers running Gerrit. By default all containers will be destroyed.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			client, err := newdockerclient(cmd)
+			if err != nil {
+				return err
+			}
+			ids := []string{}
+			for _, value := range args {
+				container, err := client.GetContainer(value)
+				if err != nil {
+					log.WithError(err).Errorf(
+						"No container found for '%s'", value)
+					continue
+				}
+				ids = append(ids, container.ID)
+			}
+
+			if len(args) == 0 {
+				containers, err := client.Containers()
+				if err != nil {
+					return err
+				}
+				for _, container := range containers {
+					ids = append(ids, container.ID)
+				}
+			}
+
+			for _, id := range ids {
+				if err := client.RemoveContainer(id); err != nil {
+					log.WithError(err).Errorf("Failed to remove %s", err)
+					continue
+				}
+				fmt.Println(id)
+			}
+
+			return nil
+		}}
 )
 
 func printinfo(container *gerrittest.Container) {
@@ -91,8 +132,10 @@ func init() {
 		"log-level", "", "Override the default log level.")
 
 	Command.AddCommand(ListCommand)
-
 	Command.AddCommand(RunCommand)
+	Command.AddCommand(DestroyCommand)
+
+	// Run flags
 	RunCommand.Flags().Int(
 		"port-http", 0,
 		"If provided run Gerrit's HTTP service on this port. A random "+
