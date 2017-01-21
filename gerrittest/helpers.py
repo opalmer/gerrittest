d16dd59b9d0ff39c5381a2bcad58dce8782be991
Oliver Palmer <oliverpalmer@opalmer.com>
2017-01-21 17:37:11 -0500

    added add_rsa_key

--- gerrittest/helpers.py
+++ gerrittest/helpers.py
@@ -1,19 +1,20 @@
 import tempfile
 import os
+import time
 from os.path import join
 
 import requests
-from requests.cookies import RequestsCookieJar
 from requests.auth import HTTPDigestAuth
+from requests.cookies import RequestsCookieJar
+from requests.exceptions import  ConnectionError
 
 from gerrittest.logger import logger
 from gerrittest.command import check_output
 
-AUTH_ADMIN = HTTPDigestAuth("admin", "secret")
-
 
 def create_admin(address, port):
     """Creates the admin account and returns (username, secret)"""
+    logger.debug("Creating admin account.")
     cookies = RequestsCookieJar()
     base_url = "http://{address}:{port}".format(address=address, port=port)
 
@@ -24,7 +25,7 @@ def create_admin(address, port):
 
     # Try to login with the newly created admin account.
     url = "{}/a/accounts/self".format(base_url)
-    response = requests.get(url, auth=AUTH_ADMIN)
+    response = requests.get(url, auth=HTTPDigestAuth("admin", "secret"))
     logger.debug("GET %s (response: %s)", url, response.status_code)
     response.raise_for_status()
 
@@ -33,6 +34,7 @@ def create_admin(address, port):
 
 def generate_rsa_key():
     """Generates an RSA key for ssh. Returns the generated key."""
+    logger.debug("Generating RSA key.")
     dirname = tempfile.mkdtemp()
     path = join(dirname, "id_rsa")
 
@@ -41,5 +43,34 @@ def generate_rsa_key():
     command = 'ssh-keygen -b 2048 -t rsa -f %s -q -N ""' % path
     logger.debug(command)
     os.system(command)
-
     return path
+
+
+def add_rsa_key(address, http_port, ssh_port, username, password, key_path):
+    """
+    Adds the ssh key provided by ``key_path`` to the requested user's
+    account.
+    """
+    logger.debug("Adding RSA key %s to %s", key_path, username)
+    url = "http://{address}:{port}/a/accounts/self/sshkeys".format(
+        address=address, port=http_port)
+    cookies = RequestsCookieJar()
+
+    logger.debug("POST %s", url)
+    with open(key_path + ".pub", "rb") as key:
+        response = requests.post(
+            url,
+            data=key, cookies=cookies,
+            auth=HTTPDigestAuth(username, password))
+        response.raise_for_status()
+
+    check_output([
+        "ssh",
+        "-o", "LogLevel=quiet",
+        "-o", "UserKnownHostsFile=%s" % os.devnull,
+        "-o", "StrictHostKeyChecking=no",
+        "-i", key_path,
+        "-p", str(ssh_port),
+        "admin@%s" % address,
+        "gerrit", "version"
+    ])
