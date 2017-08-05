693508c06557711d99c04ff1c949c54c7ccb597a
Oliver Palmer <oliverpalmer@opalmer.com>
2017-08-05 16:08:41 -0400

    removing python based code, consumers of gerrittest are using Go

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ex
-
-container_id=$(gerrittest --log-level debug run)
-gerrittest --log-level debug wait $container_id
-gerrittest --log-level debug get-port ssh $container_id
-gerrittest --log-level debug get-port http $container_id
-gerrittest --log-level debug create-admin $container_id
-gerrittest --log-level debug kill --all
