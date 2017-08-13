ecf30327f93f8035709610a9789aef445c9e6e6d
Oliver Palmer <oliverpalmer@opalmer.com>
2017-08-12 22:23:21 -0400

    NewConfig not necessary

--- config.go
+++ config.go
@@ -1,11 +1,6 @@
 package gerrittest
 
-import (
-	"github.com/opalmer/dockertest"
-	"golang.org/x/crypto/ssh"
-)
-
-// Config is used to tell the *Service struct what setup steps
+// Config is used to tell the *runner struct what setup steps
 // to perform, where to listen for services, etc.
 type Config struct {
 	// Image is the name of docker image to run.
@@ -17,32 +12,7 @@ type Config struct {
 	// PortHTTP is the port to expose the HTTP service on.
 	PortHTTP uint16
 
-	// CreateAdmin when true will cause a default admin
-	// account to be created.
-	CreateAdmin bool
-
-	// Keep indicates if the container should be kept around
+	// CleanupOnFailure indicates if the container should be kept around
 	// after we're done and/or after failure.
-	Keep bool
-
-	// PublicKey is the key to use to access the service as
-	// admin.
-	PublicKey ssh.PublicKey
-
-	// PrivateKeyPath is used internally by gerrittest.
-	PrivateKeyPath string
-}
-
-// NewConfig produces a *Config struct with reasonable
-// default values.
-func NewConfig() *Config {
-	return &Config{
-		Image:          "opalmer/gerrittest:latest",
-		PortSSH:        dockertest.RandomPort,
-		PortHTTP:       dockertest.RandomPort,
-		CreateAdmin:    true,
-		Keep:           false,
-		PublicKey:      nil,
-		PrivateKeyPath: "",
-	}
+	CleanupOnFailure bool
 }
