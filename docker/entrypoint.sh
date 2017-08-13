f05c65c22375342d313ec60433e2a302656a7dc2
Oliver Palmer <oliverpalmer@opalmer.com>
2017-08-12 21:52:51 -0400

    improvements to docker image

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,4 @@ set_gerrit_config() {
 }
 
 set_gerrit_config gerrit.canonicalWebUrl ${GERRIT_CANONICAL_URL}
-
-java -jar ${GERRIT_HOME}/gerrit.war init --batch --no-auto-start -d ${GERRIT_SITE}
-exec java -jar ${GERRIT_HOME}/gerrit.war daemon --console-log -d ${GERRIT_SITE}
-
+exec java -jar ${GERRIT_SITE}/bin/gerrit.war daemon --console-log -d ${GERRIT_SITE}
