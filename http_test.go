f04379836fc61de64f317bfc3d605542052a0358
Oliver Palmer <oliverpalmer@opalmer.com>
2017-08-13 15:44:04 -0400

    expanding tests

--- http_test.go
+++ http_test.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"net/http"
 
+	"net/http/httptest"
+
+	"io/ioutil"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/opalmer/dockertest"
 	. "gopkg.in/check.v1"
@@ -35,6 +39,13 @@ func newService(c *C) *Service {
 	}
 }
 
+func newClient(c *C) *HTTPClient {
+	svc := newService(c)
+	client, err := NewHTTPClient(svc, "foobar")
+	c.Assert(err, IsNil)
+	return client
+}
+
 func (s *HTTPTest) TestNewHTTPClient(c *C) {
 	svc := newService(c)
 	client, err := NewHTTPClient(svc, "foobar")
@@ -47,4 +58,36 @@ func (s *HTTPTest) TestNewHTTPClient(c *C) {
 		log:  log.WithField("cmp", "http"),
 	}
 	c.Assert(client, DeepEquals, expected)
+}
+
+func (s *HTTPTest) TestURL(c *C) {
+	client := newClient(c)
+	client.Prefix = "https://localhost"
+	c.Assert(client.URL("/foobar/"), Equals, "https://localhost/foobar/")
+}
+
+func (s *HTTPTest) TestNewRequest(c *C) {
+	client := newClient(c)
+	request, err := client.NewRequest(http.MethodDelete, "/foo", nil)
+	c.Assert(err, IsNil)
+	expected, err := http.NewRequest(http.MethodDelete, client.URL("/foo"), nil)
+	expected.Header.Add("X-User", client.User)
+	c.Assert(err, IsNil)
+	c.Assert(request, DeepEquals, expected)
+}
+
+func (s *HTTPTest) TestDo(c *C) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "foo")
+	}))
+	defer ts.Close()
+	client := newClient(c)
+	client.Prefix = ts.URL
+	request, err := client.NewRequest(http.MethodGet, "/", nil)
+	c.Assert(err, IsNil)
+	response, err := client.Do(request, http.StatusOK)
+	c.Assert(err, IsNil)
+	body, err := ioutil.ReadAll(response.Body)
+	response.Body.Close()
+	c.Assert(string(body), Equals, "foo")
 }
