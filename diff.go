78ce57b996f9b5161211ee1b6af95c207af77bf3
Oliver Palmer <oliverpalmer@opalmer.com>
2017-08-26 16:20:03 -0400

    added ApplyToRepository(), more logging

--- diff.go
+++ diff.go
@@ -12,6 +12,7 @@ type Diff struct {
 	// Error should be set whenever
 	Error   error
 	Content []byte
+	Commit  string
 }
 
 // ApplyToRoot will apply the given diff to the provided repository root.
@@ -19,23 +20,50 @@ func (d *Diff) ApplyToRoot(root string) error {
 	if d.Error != nil {
 		return d.Error
 	}
-
+	logger := log.WithFields(log.Fields{
+		"phase":  "apply-diff",
+		"patch":  string(d.Content),
+		"action": "apply-to-root",
+	})
 	cmd := exec.Command(
-		"git", "-C", root, "apply")
+		"git", "-C", root, "apply", "-v")
 	writer, err := cmd.StdinPipe()
 	if err != nil {
+		logger.WithError(err).Error()
 		return err
 	}
 	if _, err := writer.Write(d.Content); err != nil {
+		logger.WithError(err).Error()
 		return err
 	}
 	if err := writer.Close(); err != nil {
+		logger.WithError(err).Error()
 		return err
 	}
 	out, err := cmd.CombinedOutput()
+	logger = logger.WithField("output", string(out))
 	if err != nil {
-		log.WithError(err).WithField("out", string(out)).Error()
+		logger.WithError(err).Error()
 		return err
 	}
 	return nil
+}
+
+// ApplyToRepository takes the diff and applies it to the given
+// repository using ApplyToRoot() after which this function will amend
+// the current commit. It's up to the caller to push the change.
+func (d *Diff) ApplyToRepository(repo *Repository) error {
+	if err := d.ApplyToRoot(repo.Root); err != nil {
+		return err
+	}
+	logger := log.WithFields(log.Fields{
+		"phase":  "apply-diff",
+		"action": "add-all",
+	})
+	if _, _, err := repo.Run([]string{"add", "--all"}); err != nil {
+		logger.WithError(err).Error()
+		return err
+	}
+	logger.Debug()
+	return repo.Amend()
 }
