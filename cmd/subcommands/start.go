1d270eede1c48e379b834d04c677e03caf55a34a
Oliver Palmer <oliverpalmer@opalmer.com>
2017-09-03 16:20:25 -0400

    refactored start a bit so it can more easily be tested

--- cmd/subcommands/start.go
+++ cmd/subcommands/start.go
@@ -11,71 +11,77 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func addStartFlags(cmd *cobra.Command) {
+	cmd.Flags().Duration(
+		"timeout", time.Minute*2,
+		"The maximum amount of time to wait for the service to come up.")
+	cmd.Flags().BoolP(
+		"no-cleanup", "n", false,
+		"If provided then do not cleanup the container on failure. "+
+			"Useful when debugging changes to the docker image.")
+	cmd.Flags().String(
+		"json", "",
+		"The location to write information about the service to. Any "+
+			"existing content will be overwritten.")
+	cmd.Flags().String(
+		"image", gerrittest.DefaultImage,
+		"The Docker image to spin up Gerrit.")
+	cmd.Flags().Uint16(
+		"port-http", dockertest.RandomPort,
+		"The local port to map to Gerrit's REST API. Random by default.")
+	cmd.Flags().Uint16(
+		"port-ssh", dockertest.RandomPort,
+		"The local port to map to Gerrit's REST API. Random by default.")
+	cmd.Flags().StringP(
+		"private-key", "i", "",
+		"If provided then use this private key instead of generating one.")
+	cmd.Flags().Bool(
+		"start-only", false,
+		"If provided just start the container, don't setup anything else.")
+	cmd.Flags().StringP(
+		"password", "p", "",
+		"If provided then use this value for the admin password instead "+
+			"of generating one.")
+}
+
+func newStartConfig(cmd *cobra.Command) *gerrittest.Config {
+	// Setup timeout and Ctrl+C handling.
+	timeout := getDuration(cmd, "timeout")
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	interrupts := make(chan os.Signal, 1)
+	signal.Notify(interrupts, os.Interrupt)
+	go func() {
+		defer cancel()
+		for range interrupts {
+			return
+		}
+	}()
+
+	config := gerrittest.NewConfig()
+	config.Image = getString(cmd, "image")
+	config.PortSSH = getUInt16(cmd, "port-ssh")
+	config.PortHTTP = getUInt16(cmd, "port-http")
+	config.PrivateKey = getString(cmd, "private-key")
+	config.Password = getString(cmd, "password")
+	config.Context = ctx
+	config.SkipSetup = getBool(cmd, "start-only")
+	config.SkipCleanup = getBool(cmd, "no-cleanup")
+	return config
+}
+
 // Start implements the `start` subcommand.
 var Start = &cobra.Command{
 	Use:   "start",
 	Short: "Responsible for starting an instance of Gerrit.",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// Setup timeout and Ctrl+C handling.
-		timeout := getDuration(cmd, "timeout")
-		ctx, cancel := context.WithTimeout(context.Background(), timeout)
-		interrupts := make(chan os.Signal, 1)
-		signal.Notify(interrupts, os.Interrupt)
-		go func() {
-			defer cancel()
-			for range interrupts {
-				return
-			}
-		}()
-
-		config := gerrittest.NewConfig()
-		config.Image = getString(cmd, "image")
-		config.PortSSH = getUInt16(cmd, "port-http")
-		config.PortHTTP = getUInt16(cmd, "port-http")
-		config.PrivateKey = getString(cmd, "private-key")
-		config.Password = getString(cmd, "password")
-		config.Context = ctx
-		config.SkipSetup = getBool(cmd, "start-only")
-		config.SkipCleanup = getBool(cmd, "no-cleanup")
-
-		gerrit, err := gerrittest.New(config)
+		gerrit, err := gerrittest.New(newStartConfig(cmd))
 		if err != nil {
 			return err
 		}
-
 		return jsonOutput(cmd, gerrit)
 	},
 }
 
 func init() {
-	Start.Flags().Duration(
-		"timeout", time.Minute*2,
-		"The maximum amount of time to wait for the service to come up.")
-	Start.Flags().BoolP(
-		"no-cleanup", "n", false,
-		"If provided then do not cleanup the container on failure. "+
-			"Useful when debugging changes to the docker image.")
-	Start.Flags().String(
-		"json", "",
-		"The location to write information about the service to. Any "+
-			"existing content will be overwritten.")
-	Start.Flags().String(
-		"image", gerrittest.DefaultImage,
-		"The Docker image to spin up Gerrit.")
-	Start.Flags().Uint16(
-		"port-http", dockertest.RandomPort,
-		"The local port to map to Gerrit's REST API. Random by default.")
-	Start.Flags().Uint16(
-		"port-ssh", dockertest.RandomPort,
-		"The local port to map to Gerrit's REST API. Random by default.")
-	Start.Flags().StringP(
-		"private-key", "i", "",
-		"If provided then use this private key instead of generating one.")
-	Start.Flags().Bool(
-		"start-only", false,
-		"If provided just start the container, don't setup anything else.")
-	Start.Flags().StringP(
-		"password", "p", "",
-		"If provided then use this value for the admin password instead "+
-			"of generating one.")
+	addStartFlags(Start)
 }
--- cmd/subcommands/start_test.go
+++ cmd/subcommands/start_test.go
@@ -1,9 +1,41 @@
 package cmd
 
 import (
+	"github.com/spf13/cobra"
 	. "gopkg.in/check.v1"
 )
 
 type StartTest struct{}
 
 var _ = Suite(&StartTest{})
+
+func (s *StartTest) Test_addStartFlags(c *C) {
+	command := &cobra.Command{}
+	addStartFlags(command)
+	c.Assert(command.Flag("password"), NotNil)
+}
+
+func (s *StartTest) Test_newStartConfig(c *C) {
+	command := &cobra.Command{}
+	addStartFlags(command)
+	c.Assert(command.ParseFlags(
+		[]string{
+			"--image=image",
+			"--port-http=1",
+			"--port-ssh=2",
+			"--private-key=private-key",
+			"--password=password",
+			"--no-cleanup",
+			"--start-only",
+		}),
+		IsNil)
+	cfg := newStartConfig(command)
+	c.Assert(cfg.Image, Equals, "image")
+	c.Assert(cfg.PortHTTP, Equals, uint16(1))
+	c.Assert(cfg.PortSSH, Equals, uint16(2))
+	c.Assert(cfg.PrivateKey, Equals, "private-key")
+	c.Assert(cfg.Password, Equals, "password")
+	c.Assert(cfg.Context, NotNil)
+	c.Assert(cfg.SkipSetup, Equals, true)
+	c.Assert(cfg.SkipCleanup, Equals, true)
+}
