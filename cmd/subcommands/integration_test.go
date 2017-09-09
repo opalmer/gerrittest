1418dacfdebb27339977353bdd3ff1e6dc7a771c
Oliver Palmer <oliverpalmer@opalmer.com>
2017-09-08 21:46:27 -0400

    fixing push args

--- cmd/subcommands/integration_test.go
+++ cmd/subcommands/integration_test.go
@@ -58,7 +58,8 @@ func (s *IntegrationTest) Test_StartStop(c *C) {
 	c.Assert(gerrit.Repo.AddRemoteFromContainer(gerrit.Container, "testing", "testing"), IsNil)
 	c.Assert(gerrit.Repo.AddContent("foo/bar.txt", 0600, []byte("Hello")), IsNil)
 	c.Assert(gerrit.Repo.Commit("42: hello"), IsNil) // Note, the number is an attempt to mess up Push()
-	c.Assert(gerrit.Repo.Push("", ""), IsNil)
+	c.Assert(gerrit.Repo.Push("", ""), ErrorMatches, "remote note provided")
+	c.Assert(gerrit.Repo.Push("testing", ""), IsNil)
 	change, err := gerrit.Repo.ChangeID()
 	c.Assert(err, IsNil)
 
