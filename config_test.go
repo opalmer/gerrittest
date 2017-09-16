72c96c41149055f8c364a0a222a637ab8d9d1d17
Oliver Palmer <oliverpalmer@opalmer.com>
2017-09-15 21:26:25 -0400

    add tests for projectConfig

--- config_test.go
+++ config_test.go
@@ -2,7 +2,9 @@ package gerrittest
 
 import (
 	"os"
+	"io/ioutil"
 
+	"github.com/go-ini/ini"
 	. "gopkg.in/check.v1"
 )
 
@@ -32,4 +34,46 @@ func (s *ConfigTest) TestNewConfigOverride(c *C) {
 	c.Assert(os.Setenv(DefaultImageEnvironmentVar, "override"), IsNil)
 	cfg := NewConfig()
 	c.Assert(cfg.Image, Equals, "override")
+}
+
+func (s *ConfigTest) testWrittenConfig(c *C, path string) {
+	cfg, err := ini.LoadSources(ini.LoadOptions{AllowShadows: true}, path)
+	c.Assert(err, IsNil)
+	heads := cfg.Section(accessHeads)
+	c.Assert(
+		heads.Key("label-Verified").ValueWithShadows(), DeepEquals,
+		[]string{"-1..+1 group Administrators", "-1..+1 group Project Owners"})
+	verifiedLabel := cfg.Section(labelVerified)
+	c.Assert(verifiedLabel.Key("function").Value(), Equals, "MaxWithBlock")
+	c.Assert(verifiedLabel.Key("defaultValue").Value(), Equals, "0")
+	c.Assert(
+		verifiedLabel.Key("value").ValueWithShadows(), DeepEquals,
+		[]string{"-1 Fails", "0 No Score", "+1 Verified"})
+}
+
+func (s *ConfigTest) Test_projectConfig_newProjectConfig_missingFile(c *C) {
+	file, err := ioutil.TempFile("", "")
+	defer os.Remove(file.Name()) // nolint: errcheck
+	c.Assert(err, IsNil)
+	c.Assert(file.Close(), IsNil)
+	c.Assert(os.Remove(file.Name()), IsNil)
+	cfg, err := newProjectConfig(file.Name())
+	c.Assert(err, IsNil)
+	c.Assert(cfg.write(file.Name()), IsNil)
+	s.testWrittenConfig(c, file.Name())
+}
+
+func (s *ConfigTest) Test_projectConfig_newProjectConfig_existingConifg(c *C) {
+	file, err := ioutil.TempFile("", "")
+	file.WriteString(`
+[label "Verified"]
+default = 1
+	`)
+	defer os.Remove(file.Name()) // nolint: errcheck
+	c.Assert(err, IsNil)
+	c.Assert(file.Close(), IsNil)
+	cfg, err := newProjectConfig(file.Name())
+	c.Assert(err, IsNil)
+	c.Assert(cfg.write(file.Name()), IsNil)
+	s.testWrittenConfig(c, file.Name())
 }
