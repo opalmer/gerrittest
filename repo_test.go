9961bd77d02084f929b5abb7a40a0b866255eafd
Oliver Palmer <oliverpalmer@opalmer.com>
2017-09-17 12:19:18 -0400

    adding tests for new Repo implementation

--- repo_test.go
+++ repo_test.go
@@ -5,6 +5,7 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/opalmer/dockertest"
 	. "gopkg.in/check.v1"
 )
 
@@ -63,6 +64,54 @@ func (s *RepoTest) TestCommitAndAmend(c *C) {
 	repo := s.newRepository(c)
 	c.Assert(repo.Commit("hello"), IsNil)
 	c.Assert(repo.Amend(), IsNil)
+}
+
+func (s *RepoTest) TestGetRemote_ErrRemoteDoesNotExist(c *C) {
+	repo := s.newRepository(c)
+	_, err := repo.GetRemote("test")
+	c.Assert(err, ErrorMatches, ErrRemoteDoesNotExist.Error())
+}
+
+func (s *RepoTest) TestAddRemote_DoesNotExist(c *C) {
+	repo := s.newRepository(c)
+	c.Assert(repo.AddRemote("test", "http://localhost/"), IsNil)
+	remote, err := repo.GetRemote("test")
+	c.Assert(err, IsNil)
+	c.Assert(remote, Equals, "http://localhost/")
+}
+
+func (s *RepoTest) TestAddRemote_ErrRemoteDiffers(c *C) {
+	repo := s.newRepository(c)
+	c.Assert(repo.AddRemote("test", "http://localhost/"), IsNil)
+	c.Assert(repo.AddRemote("test", "http://localhost2/"), ErrorMatches, ErrRemoteDiffers.Error())
+}
+
+func (s *RepoTest) TestAddOriginFromContainer(c *C) {
+	repo := s.newRepository(c)
+	container := &Container{
+		SSH: &dockertest.Port{
+			Address: "0.0.0.0",
+			Public:  2222,
+		},
+	}
+	c.Assert(repo.AddOriginFromContainer(container, "foo"), IsNil)
+	remote, err := repo.GetRemote("origin")
+	c.Assert(err, IsNil)
+	c.Assert(remote, Equals, "ssh://admin@0.0.0.0:2222/foo")
+}
+
+func (s *RepoTest) TestChangeID_ErrNoCommits(c *C) {
+	repo := s.newRepository(c)
+	_, err := repo.ChangeID()
+	c.Assert(err, ErrorMatches, ErrNoCommits.Error())
+}
+
+func (s *RepoTest) TestChangeID(c *C) {
+	repo := s.newRepository(c)
+	c.Assert(repo.Commit("foo"), IsNil)
+	change, err := repo.ChangeID()
+	c.Assert(err, IsNil)
+	c.Assert(len(change), Not(Equals), 0)
 }
 
 func (s *RepoTest) TestDestroy(c *C) {
